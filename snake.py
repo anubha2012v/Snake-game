8a529a73c3943a43578e76c7822fc8c7eb0963b0
Anubhav Singh <42691802+anubha2012v@users.noreply.github.com>
2020-12-08 14:53:01 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -16,6 +16,7 @@ def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
